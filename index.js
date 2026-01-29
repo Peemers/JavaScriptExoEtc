68ceb7bc1c1e6dc5224dfc9fab21d5be5624a963
Math Peeters <pmathieu.peeters@gmail.com>
2026-01-29 11:04:45 +0100

    mise en place script, demoConsole etc

---
+++ index.js
@@ -0,0 +1,26 @@
+
+
+
+console.log('Happy developing nondidoum ✨')
+console.warn("Affichage d'avertissement")
+console.error("Affichage d'erreur")
+console.clear("pas besoin de faire un dessin")
+console.log("Happy developing nondidoum ✨")
+console.warn("Affichage d'avertissement")
+console.error("Affichage d'erreur")
+console.dir('item')
+
+const personne = {
+  lastname: "John",
+  firstname: "Doe"
+};
+
+console.dir(personne);
+
+console.table(personne);
+
+console.time("counter1")
+setTimeout(() => {
+ console.timeEnd("counter1")
+}, 4000);
+
