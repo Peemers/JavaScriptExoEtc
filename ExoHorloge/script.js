69a4a28818560f4919733ce2d09fcac92e4e715b
Math Peeters <pmathieu.peeters@gmail.com>
2026-01-31 11:44:57 +0100

    avant modif

--- ExoHorloge/script.js
+++ ExoHorloge/script.js
@@ -1,5 +1,7 @@
 ﻿const h1 = document.getElementById("h1")
 const couleurs = ["#FF8A65", "#FFB399", "#FFD1A9", "#FFE5CC", "#FFCC99", "#FFAB91"]
+const btnParis = document.getElementById("btnParis")
+const btnMoscou = document.getElementById("btnMoscou")
 
 let index = 0;
 
@@ -10,15 +12,16 @@ function changerCouleur(){
   index = 0;
  }
 }
-setInterval(changerCouleur, 1000);
+//setInterval(changerCouleur, 1000);
 
-function horlogeNow()
+function horlogeParis()
 {
  const timeNow = new Date();
 
  h1.textContent = timeNow.toLocaleString(
    navigator.language,
    {
+    timeZone: "Europe/Paris",
     weekday: "long",
     day: "2-digit",
     month: "long",
@@ -30,9 +33,32 @@ function horlogeNow()
    }
  );
 }
+setInterval(horlogeParis, 1000);
+
+
+function horlogeMoscou()
+{
+ const timeNow = new Date();
+
+ h1.textContent = timeNow.toLocaleString(
+   navigator.language,
+   {
+    timeZone: "Europe/Moscow",
+    weekday: "long",
+    day: "2-digit",
+    month: "long",
+    year: "numeric",
+    hour: "2-digit",
+    minute: "2-digit",
+    second: "2-digit",
+    millisecond: "2-digit",
+   }
+ );
+}
+setInterval(horlogeMoscou, 1000);
 
 h1.addEventListener("mouseover", function(){
- h1.style.transform = "scale(1.2)";
+ h1.style.transform = "scale(1.05)";
  h1.style.transition = "transform 0.3s ease-in";
 });
 
@@ -40,7 +66,15 @@ h1.addEventListener("mouseout", function(){
  h1.style.transform = "scale(1)";
 });
 
-setInterval(horlogeNow, 1000);
+btnParis.addEventListener("click", function(){
+ horlogeParis();
+});
+
+btnMoscou.addEventListener("click", function(){
+ horlogeMoscou();
+})
+
+
 
 
 
